f759346f5baba7d466935ac7954661ec6420f497
Brett Hayes <bretthayes@live.ca>
2017-03-27 00:38:22 -0400

    update

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import {Appbar, Button, Container, Row, Col} from 'muicss/react';
 
 export default () => (
-  <Appbar className="mui--appbar-height mui--appbar-line-height" style={{textAlign: 'center'}}>
+  <Appbar className="mui--appbar-height mui--appbar-line-height" style={{textAlign: 'center', backgroundColor: '#e07474'}}>
     <Container fluid={true}>
       <Row>
         <Col xs={12} sm={4}>
